c1d1c2a08a9439d3b0a00a10ea74368d39293ef0
Christopher Ezike <synergyaffirm@gmail.com>
2021-06-27 17:28:58 +0100

    added multisig wallet

--- MultiSigWallet/MultiSigWallet.sol
+++ MultiSigWallet/MultiSigWallet.sol
@@ -2,31 +2,27 @@ pragma solidity >=0.4.0 <0.9.0;
 
 contract MultiSigWallet {
     event Deposit(address indexed sender, uint amount, uint balance)
-    event submitTransaction(
+    event SubmitTransaction(
         address indexed owner,
         uint indexed txIndex,
         uint value,
         address indexed to,
         bytes data,
     );
-    event confirmTransaction(address indexed owner, uint indexed txIndex);
-    event executeTransaction(address indexed owner, uint indexed txIndex);
-    event revokeTransaction(address indexed owner, uint indexed txIndex);
+    event ConfirmTransaction(address indexed owner, uint indexed txIndex);
+    event ExecuteTransaction(address indexed owner, uint indexed txIndex);
+    event RevokeTransaction(address indexed owner, uint indexed txIndex);
     address [] public owners;
-    uint public numConfirmationsRequired;
+    uint public numConfirmationRequired;
 
     struct Transaction {
-        accountNUmber to;
-        debit/credit executed;
-        signee numConfirmation;
-        transactionid data;
-        egomoney value;
-        mapping(address => bool) isConfirmed;
+        address to;
+        bool executed;
         uint numConfirmation;
-    }
-    Transactions[] public transactions
-    constructor(address[] memory _owners, uint _numConfirmationsRequired) {
-        require(_owners.length > 0, "owners must decide);
+        bytes data;
+        uint value;
+        mapping(address => bool) isConfirmed;
+        uint numConfirmations;
     }
 
 
